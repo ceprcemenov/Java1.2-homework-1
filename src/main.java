d710b4b98135d48abb427fe09f17301320c49072
Sergey Semenov <gzuppy@gmail.com>
2020-02-16 17:29:25 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        long deposit = 2_000_000_000;
+        int incoming_transfer = 500_000_000;
+        long deposit_new = deposit + incoming_transfer;
+        System.out.println("Текущий баланс: " + deposit_new);
+    }
+}
